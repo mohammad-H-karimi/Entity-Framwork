c44f76a13148c7a2e8b15bb06758e579a58f8ce7
mohammad-H-karimi <127038460+mohammad-H-karimi@users.noreply.github.com>
2025-10-18 14:08:00 +0330

    Update DBContext.cs

--- DBContext.cs
+++ DBContext.cs
@@ -23,28 +23,28 @@ namespace Entity_Framework
             optionsBuilder.UseSqlServer("Server=DESKTOP-AJ8D32P\\SQLEXPRESS;Database=Entity-Framework;Trusted_Connection=True;TrustServerCertificate=True;");
        
         }
-        //DESKTOP-AJ8D32P\SQLEXPRESS
+    
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // رابطه چند به چند بین Book و Author
+            
             modelBuilder.Entity<Book>()
                 .HasMany(b => b.Authors)
                 .WithMany(a => a.Books)
                 .UsingEntity(j => j.ToTable("BookAuthors"));
 
-            // رابطه یک به یک بین Member و MembershipCard
+        
             modelBuilder.Entity<Member>()
                 .HasOne(m => m.MembershipCard)
                 .WithOne(c => c.Member)
                 .HasForeignKey<MembershipCard>(c => c.MemberId);
 
-            // رابطه یک به چند بین Member و BookLoan
+            
             modelBuilder.Entity<BookLoan>()
                 .HasOne(bl => bl.Member)
                 .WithMany(m => m.BookLoans)
                 .HasForeignKey(bl => bl.MemberId);
 
-            // رابطه یک به چند بین Book و BookLoan
+        
             modelBuilder.Entity<BookLoan>()
                 .HasOne(bl => bl.Book)
                 .WithMany()
@@ -52,3 +52,4 @@ namespace Entity_Framework
         }
     }
 }
+
